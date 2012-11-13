6ed774d094a438bf3cd9ecda9eebbc31725a0fe4
Chris Evans <gcrehl@gmail.com>
2012-11-13 14:33:16 +0000

    Fixed a couple of typos in course.py

--- src/course.py
+++ src/course.py
@@ -6,11 +6,11 @@ class course:
 	def __init__(self, name):
 		self.name = name
 	
-	def __str__(self)
+	def __str__(self):
 		return self.name
 
 	def addModule(self, module):
 		self.moduleList.append(module)
-		
+	
 	def listModules(self):
 		return '\n'.join(map(str, self.moduleList))
