0f08e8e0c77681b59938759e2af866bd74869ee3
Chris Evans <gcrehl@gmail.com>
2012-11-13 14:39:24 +0000

    Switched course.py to use spaces instead of tabs.

--- src/course.py
+++ src/course.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
-class course:
-	moduleList = []
-	
-	def __init__(self, name):
-		self.name = name
-	
-	def __str__(self):
-		return self.name
+class Course:
+    moduleList = []
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
 
-	def addModule(self, module):
-		self.moduleList.append(module)
-	
-	def listModules(self):
-		return '\n'.join(map(str, self.moduleList))
+    def addModule(self, module):
+        self.moduleList.append(module)
+    
+    def listModules(self):
+        return '\n'.join(map(str, self.moduleList))
