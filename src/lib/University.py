bafbe2c60dfd8028c1aa70b586c9ab12ed5bb896
Chris Malton <chrism@cmalton.me.uk>
2012-11-19 19:12:39 +0000

    Fix up University to actually behave itself.

--- src/lib/University.py
+++ src/lib/University.py
@@ -1,16 +1,16 @@
 import Course
 
 class University:
-	courseList = []
-	
-	def __init__(self, id, name):
-		self.id = id
-		self.name = name
-		
-	def addCourse(self, course):
-		if not isinstance(course, Course):
-			raise TypeError("course is not an instance of the Course object")
-		self.courseList.append(course)
+    
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.course_list = list()
+        
+    def addCourse(self, course):
+        if not isinstance(course, Course):
+            raise TypeError("course is not an instance of the Course object")
+        self.course_list.append(course)
 
-	def __str__(self):
-		return name
+    def __str__(self):
+        return self.name
