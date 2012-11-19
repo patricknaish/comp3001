d053210e23c59a92e36bc0eb99e99e4e7f15ce47
Chris Malton <chrism@cmalton.me.uk>
2012-11-19 18:24:14 +0000

    Restore some form of sanity to the Course (doc-strings etc)

--- src/lib/Course.py
+++ src/lib/Course.py
@@ -1,14 +1,31 @@
+"""
+Course module with class to represent a single course at a University
+"""
 class Course:
-    moduleList = []
-    
+    """
+    Class representing a course
+    """
     def __init__(self, name):
+        """
+        Initialise a new course object with a given name
+        """
         self.name = name
+        self.module_list = list()
     
     def __str__(self):
+        """
+        Return the module's name as its string representation
+        """
         return self.name
 
-    def addModule(self, module):
-        self.moduleList.append(module)
+    def add_module(self, module):
+        """
+        Add a module to this course.
+        """
+        self.module_list.append(module)
     
-    def listModules(self):
-        return '\n'.join(map(str, self.moduleList))
+    def list_modules(self):
+        """
+        Return a list of the modules part of this course.
+        """
+        return '\n'.join(map(str, self.module_list))
