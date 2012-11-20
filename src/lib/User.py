b2cda0a5ca2b7dcb9fc77a91d492c3b7c17f8d68
Chris Evans <gcrehl@gmail.com>
2012-11-20 13:36:54 +0000

    Fixed typo in User
    All lib classes should hopefully be error- and warning-free, now.

--- src/lib/User.py
+++ src/lib/User.py
@@ -31,7 +31,7 @@ class User:
         self.bookList.remove(book)
 
     def listBooks(self):
-        return '\n'.join(str(book) for book in self.bookList
+        return '\n'.join(str(book) for book in self.bookList)
 
     def __str__(self):
         return self.firstName+' '+self.lastName+' <'+self.email+'>'
