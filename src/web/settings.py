fd8c7945a82690879357dfb8dc0623652a98451d
Chris Malton <chrism@cmalton.me.uk>
2012-11-27 22:48:05 +0000

    Add some django-foo to the settings file.

--- src/web/settings.py
+++ src/web/settings.py
@@ -1,2 +1,33 @@
 # DJANGO WEB SETTINGS FILE
 
+from djangoappengine.settings_base import *
+
+import os
+
+SECRET_KEY = '' # Needs 
+
+INSTALLED_APPS = (
+    'djangoappengine',
+    'djangotoolbox',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'web', ## OUR web classes
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+)
+
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'images')
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'template'),)
+
+ROOT_URLCONF = 'urls'
