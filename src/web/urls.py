fe77d54e56bd6ebab379e4a78ddb951a89d1f7a4
Chris Malton <chrism@cmalton.me.uk>
2012-12-04 16:58:09 +0000

    Oops - forgot the URLs.py change.

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
     (r'^home$', 'web.home.render_home'),
-    (r'^register$', 'web.register.render_register')
+    (r'^register$', 'web.register.render_register'),
+    (r'^basket$', 'web.checkout.basket')
 )
