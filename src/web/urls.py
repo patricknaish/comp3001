5e19677355df130116f80ba85cddbf27e097dd5d
Chris Evans <gcrehl@gmail.com>
2012-12-04 14:30:35 +0000

    Added account to url pattern list

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
     (r'^home$', 'web.home.render_home'),
-    (r'^register$', 'web.register.render_register')
+    (r'^register$', 'web.register.render_register'),
+    (r'^account$', 'web.account.render_account')
 )
