12d66afc97207544b8a28ee881457349f4816b30
Ryan Tyrrell <infinityxii@googlemail.com>
2012-12-04 15:40:23 +0000

    updated urls.py

--- src/web/urls.py
+++ src/web/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
     (r'^home$', 'web.home.render_home'),
     (r'^register$', 'web.register.render_register'),
-    (r'^account$', 'web.account.render_account')
+    (r'^account$', 'web.account.render_account'),
+    (r'^book$', 'web.home.render_home')
 )
