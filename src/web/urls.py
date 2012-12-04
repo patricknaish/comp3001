56f28c2271e6729ecc3c4a5047783ebb92de04b0
Ryan Tyrrell <infinityxii@googlemail.com>
2012-12-04 16:05:38 +0000

    added missing comma in urls.py

--- src/web/urls.py
+++ src/web/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
-    (r'^home$', 'web.home.render_home')
+    (r'^home$', 'web.home.render_home'),
     (r'^book$', 'web.book.render_book')
 )
