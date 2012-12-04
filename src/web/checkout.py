22522fe0019be14a69ed06f870ffa371f1201dd8
Chris Malton <chrism@cmalton.me.uk>
2012-12-04 17:47:03 +0000

    Try and get the checkout working.

--- src/web/checkout.py
+++ src/web/checkout.py
@@ -1,11 +1,18 @@
 # Django magic
 import os
+from lib import Paypal
 from django.http import HttpResponse
 from django.template import Context, loader
 
 
 def render_basket(request):
-    context = Context()
+    pp = Paypal()
+    pp.sandbox = True
+    pp.merchant_id = "comp30_1354642631_biz@lists.cmalton.me.uk"
+    pp.pdt_auth_token = "lwcdLbpiHFwN8PJr08Rv6JVvYcmp90ivctfoJSWgBvANCrG-7iXJ59e8Qy4"
+    item = Item("TEST", "Test item", 10.00)
+    context = dict()
+    context["ppcheckout"] = pp.buy_now_button(item)
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'checkout.html')
     response = HttpResponse()
     response.write(loader.render_to_string(tmpl, context))
