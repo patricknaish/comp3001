171275a48c5dd090fceaa62b80c1a1fbcabb0066
Ryan Tyrrell <infinityxii@googlemail.com>
2012-12-04 16:40:56 +0000

    added login.py, updated urls

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
     (r'^home$', 'web.home.render_home'),
-    (r'^book$', 'web.book.render_book')
+    (r'^book$', 'web.book.render_book'),
+    (r'^login$', 'web.login.render_login')
 )
