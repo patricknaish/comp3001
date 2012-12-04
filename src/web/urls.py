fa596e1e43fc32594afa75ed55aa03d682b13187
Ryan Tyrrell <infinityxii@googlemail.com>
2012-12-04 15:53:10 +0000

    added book.py and changed urls.py

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
     (r'^home$', 'web.home.render_home')
-    (r'^book$', 'web.home.render_home')
+    (r'^book$', 'web.book.render_book')
 )
