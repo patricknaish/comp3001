e3047edb9a404ae0eee709f68fcc49ca167a3163
Chris Malton <chrism@cmalton.me.uk>
2012-12-04 13:08:25 +0000

    Final go at fixing home.py

--- src/web/home.py
+++ src/web/home.py
@@ -7,4 +7,5 @@ from django.template import Context, loader
 def render_home(request):
     context = Context()
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'pagebase.html')
-    print loader.render_to_string(tmpl, context)
+    response = HttpResponse()
+    response.write(loader.render_to_string(tmpl, context))
