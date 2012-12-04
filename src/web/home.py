6107213efec9973d0fd2b13a79378a521311989c
Chris Malton <chrism@cmalton.me.uk>
2012-12-04 13:05:48 +0000

    Try and get rid of this stray Content-type header that's blatting my HTML

--- src/web/home.py
+++ src/web/home.py
@@ -2,11 +2,10 @@
 import os
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import Context
+from django.template import Context, loader
 
 def render_home(request):
     context = Context()
-    response = HttpResponse();
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'pagebase.html')
-    response.write(render(request, tmpl, context))
+    response.write(loader.render_to_string(tmpl, context))
     return response
