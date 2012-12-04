b6540cd18d5e88fb9e201d3b2e3f4b8d18266ae2
Sam Bull <sambull91@gmail.com>
2012-12-04 17:34:36 +0000

    updated urls to include create_book

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
     (r'^home$', 'web.home.render_home'),
+    (r'^create_book$', 'web.books.render_create_book'),
     (r'^register$', 'web.register.render_register')
 )
