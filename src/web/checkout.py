44b1ef74adccbe73af68f4f2470ed80eb99446f4
Chris Malton <chrism@cmalton.me.uk>
2012-12-04 16:49:25 +0000

    Add the start of a checkout system.

---
+++ src/web/checkout.py
@@ -0,0 +1,12 @@
+# Django magic
+import os
+from django.http import HttpResponse
+from django.template import Context, loader
+
+
+def render_checkout(request):
+    context = Context()
+    tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'checkout.html')
+    response = HttpResponse()
+    response.write(loader.render_to_string(tmpl, context))
+    return response
