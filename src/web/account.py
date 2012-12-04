252861fb688b9b706f23aadf101b7e47a8bd6e92
Chris Evans <gcrehl@gmail.com>
2012-12-04 14:42:54 +0000

    First try at testing the account page...

--- src/web/account.py
+++ src/web/account.py
@@ -9,3 +9,15 @@ def render_account(request):
     tmpl = os.path.join(os.path.dirname(__file__), 'template', 'account.html')
     response.write(render_to_string(tmpl, context))
     return response
+
+def render_account_test(request):
+    purchase_hist = [{"book": 
+		{"image": "url",
+		"title": "Title",
+		"price": "£10.00",
+		"author": "Bob"}]
+    context = Context("purchase_history": purchase_hist)
+    response = HttpResponse()
+    tmpl = os.path.join(os.path.dirname(__file__), 'template', 'account.html')
+    response.write(render_to_string(tmpl, context))
+    return response
--- src/web/urls.py
+++ src/web/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/home',}),
     (r'^home$', 'web.home.render_home'),
-    (r'^account$', 'web.account.render_account')
+    (r'^account$', 'web.account.render_account_test')
 )
