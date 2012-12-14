159076d2fd78d52558cf20e666f4a8b305b992cd
James Barnett <jwabarnett@gmail.com>
2012-12-14 11:26:02 +0000

    Deleted Python File

--- src/django_handler.py
+++
@@ -1,7 +0,0 @@
-# Set up our Django web environment.....
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
-
-# Get hold of a WSGI application to run
-from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
