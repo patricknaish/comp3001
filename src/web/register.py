9a1b66a89510ae17e10e30801d44d9f57842ad9d
Chris Malton <chrism@cmalton.me.uk>
2012-12-21 12:34:31 +0000

    Make registration work.

--- src/web/register.py
+++ src/web/register.py
@@ -3,15 +3,48 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.template import Context
 
+# Import the app settings
+import settings
+
+# User functions
+from lib import USER
+
+class EmailDoesntMatchError(Exception):
+    def __init__(self):
+        Exception.__init__(self, "The provided email addresses do not match.")
+
 def render_register(request):
     if request.method == 'GET':
-        tmpl = os.path.join(os.path.dirname(__file__), 'template', 'registerform.html')
-        
-    if request.method == 'POST':
-        tmpl = os.path.join(os.path.dirname(__file__), 'template', 'registerpost.html')     
+        return render_register_form(request)
 
-    context = Context()
+    if request.method == 'POST':
+        return render_register_action(request)
+
+def render_register_action(request):
+    try:
+        email = cgi.escape(request.POST['email'])
+        email2 = cgi.escape(request.POST['email2'])
+        firstname = cgi.escape(request.POST['firstname'])
+        lastname = cgi.escape(request.POST['lastname'])
+        year = int(cgi.escape(request.POST['year']))
+        # Check their email addresses match
+        if email != email2:
+            raise EmailDoesntMatchError()
+
+        # Do the creation
+        USER.create_user(email, firstname, lastname, year)
+        tmpl = os.path.join(os.path.dirname(__file__), 'template', 'registersuccess.html')
+        context = Context({"error": error})
+        response = HttpResponse()
+        response.write(render_to_string(tmpl, context))
+        return response
+
+    except EmailDoesntMatchError as e:
+        return render_register_form(request, str(e))
+
+def render_register_form(request, error = None):
+    tmpl = os.path.join(os.path.dirname(__file__), 'template', 'registerform.html')
+    context = Context({"error": error})
     response = HttpResponse()
-    
     response.write(render_to_string(tmpl, context))
     return response
