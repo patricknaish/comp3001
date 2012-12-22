b0609c91b05cdfca56edeb8d7968ba2c45497d76
Chris Malton <chrism@cmalton.me.uk>
2012-12-22 12:42:46 +0000

    Another go at fixing logins...

--- src/web/login.py
+++ src/web/login.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import Context, loader
 
-def render_login(request, origin = None):
-    context = Context({"from": origin})
+def render_login(request):
+    context = Context({"from": request.GET["origin"]})
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'login.html')
     response = HttpResponse()
     response.write(loader.render_to_string(tmpl, context))
