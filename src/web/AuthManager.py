0c9e5d4d07de50c25174edfe87f147694112e95c
Chris Malton <chrism@cmalton.me.uk>
2012-12-22 11:44:10 +0000

    Rename is_logged_in to is_authenticated.

--- src/web/AuthManager.py
+++ src/web/AuthManager.py
@@ -4,12 +4,12 @@ Authentication management for users
 
 def has_permission(request, perm_name):
     "Check if a user has a given permission"
-    if is_logged_in(request):
+    if is_authenticated(request):
         return True
     else:
         return False
 
-def is_logged_in(request):
+def is_authenticated(request):
     "Is the user logged in?"
     if "user" in request.session.keys():
         return True
