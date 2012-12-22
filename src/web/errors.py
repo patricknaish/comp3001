7d5786a12a57324661ee70455efb0b86871805e8
Chris Malton <chrism@cmalton.me.uk>
2012-12-22 12:51:12 +0000

    Fix the redirect (oops!)

--- src/web/errors.py
+++ src/web/errors.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 def permission_denied(request):
     # If they are logged in, show access denied, else login form
     if "user" in request.session.keys():
-        redirect("django.views.defaults.permission_denied")
+        return redirect("django.views.defaults.permission_denied")
     else:
-        redirect("/login?origin=%s" % request.path)
+        return redirect("/login?origin=%s" % request.path)
 
