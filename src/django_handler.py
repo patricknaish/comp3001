1642e00b51bf70e4c1d9b62b23ed28f7fee899b3
Chris Malton <chrism@cmalton.me.uk>
2012-12-23 16:35:28 +0000

    Another go at fixing pythonPath....

--- src/django_handler.py
+++ src/django_handler.py
@@ -5,6 +5,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 # Add the GAEUtilites magic
 sys.path.append(os.path.sep.join([ os.path.dirname( os.path.realpath( __file__ ) ), "gaeutilities" ]))
 sys.path.append(os.path.sep.join([ os.path.dirname( os.path.realpath( __file__ ) ), "gaeutilities", "appengine_utilities" ]))
+sys.path.append(os.path.sep.join([ os.path.dirname( os.path.realpath( __file__ ) ), "gaeutilities", "appengine_utilities", "django-middleware" ]))
 
 # Get hold of a WSGI application to run
 import django.core.handlers.wsgi
--- src/web/settings.py
+++ src/web/settings.py
@@ -11,7 +11,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django-middleware.middleware.SessionMiddleware',
+    'middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
