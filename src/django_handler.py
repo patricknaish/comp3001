2b36d022f94482a292b87d27b3f5eab29005f65f
Chris Malton <chrism@cmalton.me.uk>
2012-12-23 15:08:43 +0000

    Attempt to fix the path conundrum again.

--- src/django_handler.py
+++ src/django_handler.py
@@ -4,7 +4,7 @@ import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 # Add the GAEUtilites magic
 sys.path.append(os.path.sep.join([ os.path.dirname( os.path.realpath( __file__ ) ), "gaeutilities" ]))
-sys.path.append(os.path.sep.join([ os.path.dirname( os.path.realpath( __file__ ) ), "gaeutilities", "django-middleware" ]))
+sys.path.append(os.path.sep.join([ os.path.dirname( os.path.realpath( __file__ ) ), "gaeutilities", "appengine_utilities" ]))
 
 # Get hold of a WSGI application to run
 import django.core.handlers.wsgi
