f1fbdff2fc4094895c91883998d2e673a891ae30
Chris Malton <chrism@cmalton.me.uk>
2012-12-23 10:31:13 +0000

    Fix Django Handler to use 1.3 syntax.

--- src/django_handler.py
+++ src/django_handler.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
 # Get hold of a WSGI application to run
-from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
+import django.core.handlers.wsgi
+app = django.core.handlers.wsgi.WSGIHandler()
