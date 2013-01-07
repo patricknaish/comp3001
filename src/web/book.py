dbfb1d2bc896d4b2d7246fd69c9a965657c80801
Patrick Naish <patrick@pmnaish.co.uk>
2013-01-07 10:48:21 +0000

    Can't capitalise correctly

--- src/web/book.py
+++ src/web/book.py
@@ -8,7 +8,7 @@ from lib import Book
 from lib import User
 
 def render_book(request, book_id):
-    copies = BOOK.list_book_copies(book_id)
+    copies = Book.list_book_copies(book_id)
     context = Context({"same_books":copies, "user": User.get_by_key_name(request.session["user"])})
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'book.html')
     response = HttpResponse()
