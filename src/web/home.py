e3e5de52ce065ae58005a8393f4746c9ee50e748
Patrick Naish <patrick@pmnaish.co.uk>
2013-01-07 11:33:52 +0000

    I can't write dict entries

--- src/web/home.py
+++ src/web/home.py
@@ -7,8 +7,7 @@ from django.template import Context, loader
 from lib import User
 
 def render_home(request):
-    context = Context({User.get_by_key_name(request.session["user"])})
-    print request.session["user"]
+    context = Context({"user": User.get_by_key_name(request.session["user"])})
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'home.html')
     response = HttpResponse()
     response.write(loader.render_to_string(tmpl, context))
