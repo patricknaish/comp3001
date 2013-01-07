13fd304f9264a1da014bc65065051f71277499d4
James Barnett <jwabarnett@gmail.com>
2013-01-07 18:43:04 +0000

    Started individual book listing views

--- src/web/book.py
+++ src/web/book.py
@@ -3,9 +3,14 @@ import os
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import Context, loader
+from lib import BOOK
 
-def render_book(request):
-    context = Context()
+def render_book(request, bookId):
+	try:
+		bookId = int(bookId)
+	except ValueError:
+		raise Http404();
+    context = Context({})
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'book.html')
     response = HttpResponse()
     response.write(loader.render_to_string(tmpl, context))
