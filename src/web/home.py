6c84e6afba2086145cc99167900ae14e5cb7ed56
James Barnett <jwabarnett@gmail.com>
2013-01-07 19:49:03 +0000

    Used AuthManager to check for login in home.py, rather than using sessions directly

--- src/web/home.py
+++ src/web/home.py
@@ -3,11 +3,13 @@ import os
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import Context, loader
+from web import AuthManager
 
 import lib
 
 def render_home(request):
-    context = Context({"user": lib.USER.get_by_key_name(request.session["user"])})
+	if AuthManager.is_logged_in:
+    	context = Context({"user": AuthManager.get_current_user})
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'home.html')
     response = HttpResponse()
     response.write(loader.render_to_string(tmpl, context))
