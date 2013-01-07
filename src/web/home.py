1f6fdda2805150a4aa2c4ec10a447c1419578c46
Patrick Naish <patrick@pmnaish.co.uk>
2013-01-07 11:31:29 +0000

    Output user section of session

--- src/web/home.py
+++ src/web/home.py
@@ -8,7 +8,7 @@ from lib import User
 
 def render_home(request):
     context = Context({User.get_by_key_name(request.session["user"])})
-    print context
+    print request.session["user"]
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'home.html')
     response = HttpResponse()
     response.write(loader.render_to_string(tmpl, context))
