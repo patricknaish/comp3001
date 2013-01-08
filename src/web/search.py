d7686280de396b86edf29bbcb0e750c0809408ba
James Barnett <jwabarnett@gmail.com>
2013-01-08 11:14:15 +0000

    Search now looks for the string within the book title rather than just a match

--- src/web/search.py
+++ src/web/search.py
@@ -10,7 +10,7 @@ def render_search(request, search_string):
     matched_books = []
     all_books = lib.BOOK.list_all_books #Grab all the books from the database
     for book in all_books:
-        if search_string.lower() == book.title.lower():
+        if search_string.lower() in book.title.lower():
         	matched_books.append(book)
     context = Context({ "book_list": matched_books,
     	                "user": AuthManager.get_current_user(request)})
