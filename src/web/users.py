9bb882f69c4b1c03e99450ade890c255317c43b6
James Barnett <jwabarnett@gmail.com>
2013-01-08 19:15:40 +0000

    Started messaging front end

--- src/web/users.py
+++ src/web/users.py
@@ -7,6 +7,10 @@ from web import AuthManager
 
 import lib
 
+class MessageSendError(Exception):
+    def __init__(self):
+        Exception.__init__(self, "There was an error sending this message.")
+
 def render_user(request, user_key):
     user = lib.USER.get(user_key)
     user_listings = lib.USER.list_books(user.email)
@@ -15,6 +19,34 @@ def render_user(request, user_key):
     	                "user": AuthManager.get_current_user(request)
                       })
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'user.html')
+    response = HttpResponse()
+    response.write(loader.render_to_string(tmpl, context))
+    return response
+
+def render_message(request, to_user, error = None):
+    if request.POST['message'] and request.POST['subject']:
+        return send_message(request, to_user, request.POST['message'], request.POST['subject'])
+    else :
+        context = Context({ "send_user": to_user })
+        tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'message.html')
+        response = HttpResponse()
+        response.write(loader.render_to_string(tmpl, context))
+
+def send_message(request, to_user, message, subject):
+    try:
+        lib.MESSAGE.send_message(to_user, message, subject, AuthManager.get_current_user(request))
+        tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'send_message_success.html')
+    except Exception:
+        context = Context({"error": e})
+        tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'send_message_failure.html')
+
+    response = HttpResponse()
+    response.write(loader.render_to_string(tmpl, context))
+    return response
+
+def render_inbox(request):
+    context = Context({"error": e})
+    tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'inbox.html')
     response = HttpResponse()
     response.write(loader.render_to_string(tmpl, context))
     return response
