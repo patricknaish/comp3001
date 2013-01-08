a019a27a58bb1f98e070ecfad2963481d25cd28f
Chris Malton <chrism@cmalton.me.uk>
2013-01-08 16:11:43 +0000

    Make the checkout vaguely work.

--- src/web/checkout.py
+++ src/web/checkout.py
@@ -18,11 +18,15 @@ def render_basket(request):
     pp.sandbox = True
     pp.merchant_id = "comp30_1354642631_biz@lists.cmalton.me.uk"
     pp.pdt_auth_token = "lwcdLbpiHFwN8PJr08Rv6JVvYcmp90ivctfoJSWgBvANCrG-7iXJ59e8Qy4"
-    item = lib.PAYPAL.Item("TEST", "Test item", 10.00)
     user = AuthManager.get_current_user(request)
     context = Context({"user": user})
     if 'items' in request.session.keys():
-        context["basket"] = request.session['items']
+        context["basket"] = list();
+        for item in request.session['items']:
+            userbook = lib.USERBOOK.get(item)
+            context["basket"].append(userbook)
+            total_cost = userbook.price
+        item = lib.PAYPAL.Item("TT-BASKET", "TexTrader Basket", total_cost / 100)
         context["ppcheckout"] = pp.buy_now_button(item)
         tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'checkout.html')
     else:
