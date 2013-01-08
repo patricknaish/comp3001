90cc73f9e5268d58b2f68d6a9409dee2ef2c02ac
James Barnett <jwabarnett@gmail.com>
2013-01-08 14:20:04 +0000

    Another attempt at search prediction

--- src/web/search.py
+++ src/web/search.py
@@ -61,7 +61,7 @@ def search_predict(request):
             matched_books.append(book)
     matched_objects = list()
     for book in matched_books:
-        matched_objects.append( { "label": book.title, "value": book.isbn } )
+        matched_objects.append( { label: book.title, value: book.isbn } )
     response = HttpResponse()
     response.write(json.dumps(matched_objects))
     return response
