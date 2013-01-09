12b773fff03409b7d3f801b041b4184590d757d4
Sam Bull <sambull91@gmail.com>
2013-01-09 10:58:44 +0000

    Another fix to price.py

--- src/web/templatetags/price.py
+++ src/web/templatetags/price.py
@@ -3,7 +3,7 @@ import re
 
 def to_price(var):
     price = str(var)
-    if not re.search('0+\.[0-9]*', price):
+    if not re.search('[0-9]{2}', price):
         return "0." + price[-2:]
     else:
         return price[:-2] + "." + price[-2:]
