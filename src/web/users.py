b5ca1185d88b6d821b854b5508ca8581aa9f12b8
Chris Evans <gcrehl@gmail.com>
2013-01-09 10:59:51 +0000

    Tab -> Space

--- src/web/users.py
+++ src/web/users.py
@@ -15,7 +15,7 @@ def render_user(request, user_key):
     user = lib.USER.get(user_key)
     user_listings = lib.USER.list_books(user.email)
     context = Context({ "user_listings": user_listings,
-    	                "viewing_user": user
+                        "viewing_user": user
                       })
     tmpl =  os.path.join(os.path.dirname(__file__), 'template', 'user.html')
     response = HttpResponse()
