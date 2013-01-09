23cc955bf0a3a03d64ea369bcb341c5bc95ed664
Sam Bull <sambull91@gmail.com>
2013-01-09 10:44:57 +0000

    Indentation fix

--- src/web/templatetags/price.py
+++ src/web/templatetags/price.py
@@ -3,10 +3,10 @@ import re
 
 def to_price(var):
     price = str(var)
-	if re.search('0+\.[0-9]*', 'price'):		
-		return "0." + price[-2:]
-	else:
-		return price[:-2] + "." + price[-2:]
+    if re.search('0+\.[0-9]*', 'price'):		
+        return "0." + price[-2:]
+    else:
+        return price[:-2] + "." + price[-2:]
 
 register = template.Library()
 register.filter("to_price", to_price)
